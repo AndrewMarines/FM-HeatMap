6b534b1308fa2ab78f9a60b9a93275a4e87b87f2
Andrea marini <34715958+AndrewMarines@users.noreply.github.com>
2022-05-10 14:04:33 +0200

    Screen Size
    Searching for the time bar, it will only display the right half of the screen.
    The y will now be cutted from 0+10% to y-10%

--- Utils.py
+++ Utils.py
@@ -116,7 +116,7 @@ def read_bar():
             save_config()
 
 
-    img = ImageGrab.grab(bbox=(0, 0, user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)))  # x, y, w, h.
+    img = ImageGrab.grab(bbox=(int(user32.GetSystemMetrics(0)/2), int(0 + (user32.GetSystemMetrics(1)*10)/100), user32.GetSystemMetrics(0), int(user32.GetSystemMetrics(1) - (user32.GetSystemMetrics(1)*10)/100)))  # x, y, w, h.
     img_np = np.array(img)
     RGB_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
     cv2.putText(img=RGB_img, text='CLICK THE END OF THE TIME BAR', org=(100, 100), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=3,
