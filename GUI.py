e3e081053c7aa4270af260465ba61eda7ffa012e
Andrea marini <34715958+AndrewMarines@users.noreply.github.com>
2022-05-20 11:56:57 +0200

    Folders organization

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 # Multi-frame tkinter application v2.3
+import shutil
 import tkinter as tk
 import Utils
 from PIL import Image, ImageTk
@@ -7,6 +8,7 @@ import os
 import cv2
 import time
 import easygui
+
 
 class GUI_APP(tk.Tk):
     def __init__(self):
@@ -23,24 +25,28 @@ class GUI_APP(tk.Tk):
 
         self._frame.pack()
 
+
 class Home(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         master.title("HOMEPAGE")
+
+        # SCREENSHOT SECTION
         screenshot_icon = tk.PhotoImage(file="src/screenshot.png")
         screenshot_icon = screenshot_icon.subsample(3, 3)
         ss = tk.Button(self, text='Screenshot', image=screenshot_icon,
-                    compound=tk.TOP, command=lambda:master.switch_frame(Screenshot), background="#ffffff")
+                       compound=tk.TOP, command=lambda: master.switch_frame(Screenshot), background="#ffffff")
         ss.image = screenshot_icon
         ss.pack(side=tk.LEFT)
 
+        # HEATMAP SECTION
         heatmap_icon = tk.PhotoImage(file="src/heatmap.png")
         heatmap_icon = heatmap_icon.subsample(3, 3)
         hm = tk.Button(self, text='Heatmap', image=heatmap_icon,
-                    compound=tk.TOP, command=lambda:[Utils.organize_images(), master.switch_frame(Elaborazione)], background="#ffffff")
+                       compound=tk.TOP, command=lambda: [Utils.organize_images(), master.switch_frame(Elaborazione)],
+                       background="#ffffff")
         hm.image = heatmap_icon
         hm.pack(side=tk.RIGHT)
-
 
 
 class Screenshot(tk.Frame):
@@ -49,43 +55,45 @@ class Screenshot(tk.Frame):
         master.title("SCREENSHOT")
         self.columnconfigure(1, weight=1)
         self.rowconfigure(1, weight=1)
+
         tk.Button(self, text="HOMEPAGE", command=lambda: master.switch_frame(Home), background="#ffffff").grid(
-            row=0, column=0,sticky=tk.NW)
-        tk.Label(self, text="INGAME SECONDS PER SCREENSHOT").grid(row=1, column=0, padx= 5)
+            row=0, column=0, sticky=tk.NW)
+
+        # SPINBOXES
+        tk.Label(self, text="INGAME SECONDS PER SCREENSHOT").grid(row=1, column=0, padx=5)
         var_ts = tk.IntVar(self)
         var_ts.set(Utils.ingame_seconds_per_screenshot)
-        TS = tk.Spinbox(self, from_=1, to=5000, textvariable= var_ts)
-        TS.grid(row=1, column=1, padx= 5)
+        TS = tk.Spinbox(self, from_=1, to=5000, textvariable=var_ts)
+        TS.grid(row=1, column=1, padx=5)
         var_ns = tk.IntVar(self)
-        var_ns.set(Utils. number_of_screenshot)
+        var_ns.set(Utils.number_of_screenshot)
         tk.Label(self, text="Number Of Screenshots").grid(row=2, column=0, padx=5)
-        NS = tk.Spinbox(self, from_=1, to=200,textvariable=var_ns)
+        NS = tk.Spinbox(self, from_=1, to=200, textvariable=var_ns)
         NS.grid(row=2, column=1, padx=5, pady=70)
 
+        # PREVIEW
         img = Image.fromarray(Utils.ant_screenshot())
-        img_Screenshot = ImageTk.PhotoImage(image = img)
+        img_Screenshot = ImageTk.PhotoImage(image=img)
         img_Screenshot = img_Screenshot._PhotoImage__photo.subsample(2, 2)
-        Screen = tk.Button(self, image = img_Screenshot, command =lambda: [
-                                                                        master.iconify(),
-                                                                        time.sleep(0.5),
-                                                                        Utils.find_coords(master),
-                                                                        self.master.switch_frame(Home)
-                                                                        ])
+        Screen = tk.Button(self, image=img_Screenshot, command=lambda: [
+            master.iconify(),
+            time.sleep(0.5),
+            Utils.find_coords(master),
+            self.master.switch_frame(Home)
+        ])
         Screen.image = img_Screenshot
-        Screen.grid(row = 0, column=2, padx=20, pady=20,rowspan=4)
+        Screen.grid(row=0, column=2, padx=20, pady=20, rowspan=4)
 
         avvio = tk.Button(self, text="START",
-                  command=lambda: [
-                                    Utils.set_Time_Screen(var_ts.get()),
-                                    Utils.set_Number_Screenshot(var_ns.get()),
-                                    Utils.screenshot(master),
-                                    Utils.organize_images(),
-                                    master.state('normal'),
-                                    master.switch_frame(Elaborazione)
-                                    ])
-        avvio.grid(row=5,columnspan=10, pady=10, sticky=tk.NSEW)
-
-
+                          command=lambda: [
+                              Utils.set_Time_Screen(var_ts.get()),
+                              Utils.set_Number_Screenshot(var_ns.get()),
+                              Utils.screenshot(master),
+                              Utils.organize_images(),
+                              master.state('normal'),
+                              master.switch_frame(Elaborazione)
+                          ])
+        avvio.grid(row=5, columnspan=10, pady=10, sticky=tk.NSEW)
 
 
 class Elaborazione(tk.Frame):
@@ -99,46 +107,48 @@ class Elaborazione(tk.Frame):
         self.canvas.grid_columnconfigure(0, weight=1)
         self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
         self.canvas.configure(yscrollcommand=self.vsb.set)
-        self.canvas.grid(row=0,column=0, sticky=tk.NSEW)
+        self.canvas.grid(row=0, column=0, sticky=tk.NSEW)
         self.vsb.grid(row=0, column=1, sticky=tk.NSEW)
-        self.settings.grid(row=0, column = 2, sticky=tk.NSEW)
-
-
+        self.settings.grid(row=0, column=2, sticky=tk.NSEW)
 
         self.canvas.create_window((4, 4), window=self.frame, anchor="nw",
                                   tags="self.frame")
         self.frame.bind("<Configure>", self.onFrameConfigure)
         self.populate()
 
-        tk.Button(self, text="CREATE HEATMAP", command=lambda:[
-                                                                Utils.set_Pixel_Intensity(var_pix_int.get()),
-                                                                Utils.set_Around_Pixel_Intensity(var_pix_int_vic.get()),
-                                                                Utils.readconfig(),
-                                                                self.heatmap_finale()
-
-                                                               ]).grid(row=1, columnspan=3, sticky=tk.NSEW)
+        tk.Button(self, text="CREATE HEATMAP", command=lambda: [
+            Utils.set_Pixel_Intensity(var_pix_int.get()),
+            Utils.set_Around_Pixel_Intensity(var_pix_int_vic.get()),
+            Utils.readconfig(),
+            self.heatmap_finale()
+        ]).grid(row=1, columnspan=3, sticky=tk.NSEW)
 
         var_pix_int = tk.IntVar(self)
         var_pix_int.set(Utils.intensita_pixel)
-        tk.Button(self.settings, text="HOMEPAGE", command=lambda:master.switch_frame(Home), background="#ffffff").grid(row=0, column=0, pady=5)
+        tk.Button(self.settings, text="HOMEPAGE", command=lambda: master.switch_frame(Home), background="#ffffff").grid(
+            row=0, column=0, pady=5)
 
-        tk.Label(self.settings, text =  "PIXEL INTENSITY", background="#ffffff").grid(row=1, column=0, sticky= tk.NSEW)
+        tk.Label(self.settings, text="PIXEL INTENSITY", background="#ffffff").grid(row=1, column=0, sticky=tk.NSEW)
         PI = tk.Spinbox(self.settings, from_=1, to=5, textvariable=var_pix_int, background="#ffffff")
-        PI.grid(row=2, column=0, padx=5, sticky= tk.NSEW)
+        PI.grid(row=2, column=0, padx=5, sticky=tk.NSEW)
         var_pix_int_vic = tk.IntVar(self)
         var_pix_int_vic.set(Utils.intensita_p_vicini)
-        tk.Label(self.settings, text="RANGE OF PIXEL COLOURATION", background="#ffffff").grid(row=3, column=0, sticky= tk.NSEW)
+        tk.Label(self.settings, text="RANGE OF PIXEL COLOURATION", background="#ffffff").grid(row=3, column=0,
+                                                                                              sticky=tk.NSEW)
         PI_V = tk.Spinbox(self.settings, from_=0, to=5, textvariable=var_pix_int_vic, background="#ffffff")
-        PI_V.grid(row=4, column=0, padx=5, sticky= tk.NSEW)
-        tk.Button(self.settings, text= "RELOAD SCREEN", command=self.reload, background="#ffffff").grid(row=5, column=0, sticky=tk.NSEW)
+        PI_V.grid(row=4, column=0, padx=5, sticky=tk.NSEW)
+        tk.Button(self.settings, text="RELOAD SCREEN", command=self.reload, background="#ffffff").grid(row=5, column=0,
+                                                                                                       sticky=tk.NSEW)
 
     def heatmap_finale(self):
         def salva_heatmap(img):
-            f_out=easygui.filesavebox("Select a output file",title="SAVE HEATMAP", default = "HEATMAP", filetypes= ".png")
+            f_out = easygui.filesavebox("Select a output file", title="SAVE HEATMAP", default="HEATMAP",
+                                        filetypes=".png")
             if ".png" in f_out:
                 cv2.imwrite(f_out, img)
             else:
-                cv2.imwrite(f_out+".png", img)
+                cv2.imwrite(f_out + ".png", img)
+
         top = tk.Toplevel()
         top.title("Heatmap")
         original_img = Utils.generazione_heatmap()
@@ -149,46 +159,73 @@ class Elaborazione(tk.Frame):
 
         # Create a Label to display the image
         tk.Label(top, image=img_Screenshot).grid(row=0, column=0)
-        tk.Button(top, text = "SAVE HEATMAP", command = lambda: salva_heatmap(original_img)).grid(row=1, column = 0)
+        tk.Button(top, text="SAVE HEATMAP", command=lambda: salva_heatmap(original_img)).grid(row=1, column=0)
         top.mainloop()
 
-
     def populate(self):
-            btn_img = []
-            btn_remove = []
-            btn_rem_disk =[]
-            images = glob.glob('elab_movimenti/*.png')
-            ants = []
-            padx = 10
-            pady=10
-            if(len(images) == 0):
-                self.no_screenshot()
+        btn_img = []
+        btn_remove = []
+        btn_rem_disk = []
+        path = "elab_movimenti"
+        if not os.path.exists(path + '/DEFAULT'):
+            os.mkdir(path + '/DEFAULT')
+        directories = glob.glob(path + "/*/", recursive=True)
+        all_images = glob.glob(path + "/**/*.png", recursive=True)
 
-            for i in range( len(images)):
-                ants.append(tk.PhotoImage(file=images[i]))
-                ants[i] = ants[i].subsample(3,3)
-                btn_img.append(tk.Button(self.frame, text=images[i], image=ants[i],
-                                         compound=tk.TOP, command= lambda  c=i: self.getImage(c)))
-                btn_remove.append(tk.Button(self.frame, text = "REMOVE FROM QUEUE", command = lambda c=i : [
-                                                                                                        os.remove(images[c]),
-                                                                                                        self.refresh()
-                                                                                                        ]))
-                btn_rem_disk.append(tk.Button(self.frame, text = "REMOVE FROM DISK", command = lambda c=i : [
-                                                                                                        os.remove(images[c]),
-                                                                                                        os.remove(images[c].replace("elab_","")),
-                                                                                                        self.refresh()
-                                                                                                        ]))
-                btn_img[i].image = ants[i]
-                btn_img[i].grid(row = i, column=0, sticky=tk.NS, padx = padx, pady = pady)
-                btn_remove[i].grid(row = i, column = 1, sticky=tk.NS, padx = padx, pady = pady)
-                btn_rem_disk[i].grid(row = i, column = 2, sticky=tk.NS, padx = padx, pady = pady)
+
+        ants = []
+        btn_remove_dir = []
+        padx = 10
+        pady = 10
+        index = 0
+        if len(all_images) == 0:
+            self.no_screenshot()
+        if len(directories) == 0:
+            directories.append(path)
+        for f in all_images:
+            contenuto = False
+            for d in directories:
+                if(d in f):
+                    contenuto = True
+            if(not contenuto):
+                shutil.move(f, path + '/DEFAULT')
+        all_images = glob.glob(path + "/**/*.png", recursive=True)
+        index_dir = -1
+        for d in range(len(directories)):
+            d_string = directories[d].upper()
+            d_string = d_string.replace("ELAB_MOVIMENTI\\", "").replace("\\","")
+            tk.Label(self.frame, text = d_string, background="#ffffff").grid(row = index_dir + 1 , column = 0)
+            images = glob.glob(directories[d] + '/*.png')
+
+            for i in range(len(images)):
+                ants.append(tk.PhotoImage(file=all_images[index]))
+                ants[index] = ants[index].subsample(3, 3)
+                btn_img.append(tk.Button(self.frame, text=all_images[index], image=ants[index],
+                                         compound=tk.TOP, command=lambda c=index: self.getImage(c)))
+                btn_remove.append(tk.Button(self.frame, text="REMOVE FROM QUEUE", command=lambda c=index: [
+                    os.remove(all_images[c]),
+                    self.refresh()
+                ]))
+                btn_rem_disk.append(tk.Button(self.frame, text="REMOVE FROM DISK", command=lambda c=index: [
+                    os.remove(all_images[c]),
+                    os.remove(all_images[c].replace("elab_", "")),
+                    self.refresh()
+                ]))
+                btn_img[index].image = ants[index]
+                btn_img[index].grid(row=index_dir+2, column=0, sticky=tk.NS, padx=padx, pady=pady)
+                btn_remove[index].grid(row=index_dir+2, column=1, sticky=tk.NS, padx=padx, pady=pady)
+                btn_rem_disk[index].grid(row=index_dir+2, column=2, sticky=tk.NS, padx=padx, pady=pady)
+                index += 1
+                index_dir += 2
+
 
     def onFrameConfigure(self, event):
-            '''Reset the scroll region to encompass the inner frame'''
-            self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
-    def getImage(self,i):
-        images = glob.glob('elab_movimenti/*.png')
+    def getImage(self, i):
+        path = "elab_movimenti"
+        images = glob.glob(path + "/**/*.png", recursive=True)
         image = cv2.imread(images[i])
         cv2.imshow(images[i], image)
         cv2.waitKey(0)
@@ -205,14 +242,12 @@ class Elaborazione(tk.Frame):
     def no_screenshot(self):
         top = tk.Toplevel()
         top.title("NO SCREENSHOT AVAILABLE")
-        tk.Label(top, text= """HEY BRO, SEEMS LIKE YOU HAVEN'T GOT SCREENSHOTS YET.
+        tk.Label(top, text="""HEY BRO, SEEMS LIKE YOU HAVEN'T GOT SCREENSHOTS YET.
     WHERE DO YOU WANNA GET BACK?""").grid(row=0, columnspan=2)
-        tk.Button(top, text= "HOMEPAGE", command=lambda: [self.master.switch_frame(Home), top.destroy()]).grid(row=1, column=0)
-        tk.Button(top, text= "SCREENSHOT", command=lambda: [self.master.switch_frame(Screenshot), top.destroy()]).grid(row=1, column=1)
-
-
-
-
+        tk.Button(top, text="HOMEPAGE", command=lambda: [self.master.switch_frame(Home), top.destroy()]).grid(row=1,
+                                                                                                              column=0)
+        tk.Button(top, text="SCREENSHOT", command=lambda: [self.master.switch_frame(Screenshot), top.destroy()]).grid(
+            row=1, column=1)
 
 
 def main():
--- Utils.py
+++ Utils.py
@@ -10,35 +10,37 @@ import shutil
 import mouse
 import ctypes
 import GUI
+
 user32 = ctypes.windll.user32
 
-
-intensita_pixel=3
+intensita_pixel = 3
 intensita_p_vicini = 1
 ingame_seconds_per_screenshot = 40
 number_of_screenshot = 15
 x_iniziale = 0
 y_iniziale = 0
-x_finale = 1000
-y_finale = 500
+x_finale = 10
+y_finale = 10
 x_bar = 0
 y_bar = 0
 
-lower = np.array([0,0,120])
-upper = np.array([40,170,255])
+lower = np.array([0, 0, 120])
+upper = np.array([40, 170, 255])
 CONF_FILE = "config.ini"
 config = configparser.ConfigParser()
+
+
 def get_time():
     now = datetime.now()  # current date and time
-    time = str(now.strftime("%m-%d-%Y %H-%M-%S"))
-    return time
-def screenshot(master):
+    formatted_time = str(now.strftime("%m-%d-%Y %H-%M-%S"))
+    return formatted_time
 
+
+def screenshot(master):
     read_bar(master)
     readconfig()
     x = 0
-    print(number_of_screenshot)
-    while(x<number_of_screenshot):
+    while (x < number_of_screenshot):
         print(x)
         now = datetime.now()  # current date and time
         file_name = str(now.strftime("%m-%d-%Y %H-%M-%S"))
@@ -51,11 +53,12 @@ def screenshot(master):
         time.sleep(1)
         img = ImageGrab.grab(bbox=(x_iniziale, y_iniziale, x_finale, y_finale))  # x, y, w, h.
         img_np = np.array(img)
-        RGB_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-        cv2.imwrite("movimenti/"+file_name+".png", RGB_img)
-        x+=1
+        rgb_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+        cv2.imwrite("movimenti/" + file_name + ".png", rgb_img)
+        x += 1
     path = os.path.abspath("movimenti/")
     os.startfile(path)
+
 
 def ant_screenshot():
     readconfig()
@@ -64,14 +67,13 @@ def ant_screenshot():
     RGB_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
     return RGB_img
 
-def find_coords(master):
 
+def find_coords(master):
     def click_event(event, x, y, flags, params):
         global x_finale
         global x_iniziale
         global y_finale
         global y_iniziale
-
 
         # checking for left mouse clicks
 
@@ -79,11 +81,8 @@ def find_coords(master):
             # displaying the coordinates
             # on the Shell
 
-            x_iniziale =x
+            x_iniziale = x
             y_iniziale = y
-
-
-
 
         # checking for right mouse clicks
         if event == cv2.EVENT_RBUTTONDOWN:
@@ -97,15 +96,15 @@ def find_coords(master):
 
     img = ImageGrab.grab(bbox=(0, 0, user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)))  # x, y, w, h.
     img_np = np.array(img)
-    RGB_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-    cv2.imshow('image', RGB_img)
+    rgb_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    cv2.imshow('image', rgb_img)
     cv2.setMouseCallback('image', click_event)
+
 
 def read_bar(master):
     def click_event(event, x, y, flags, params):
         global x_bar
         global y_bar
-
 
         # checking for left mouse clicks
 
@@ -113,57 +112,62 @@ def read_bar(master):
             # displaying the coordinates
             # on the Shell
             cv2.destroyWindow("image")
-            x_bar =  int(x + user32.GetSystemMetrics(0)/2)
+            x_bar = int(x + user32.GetSystemMetrics(0) / 2)
             print(x)
             print(x_bar)
-            y_bar = int(y + (user32.GetSystemMetrics(1)*10)/100)
+            y_bar = int(y + (user32.GetSystemMetrics(1) * 10) / 100)
             save_config()
 
-    img = ImageGrab.grab(bbox=(int(user32.GetSystemMetrics(0)/2), int(0 + (user32.GetSystemMetrics(1)*10)/100), user32.GetSystemMetrics(0), int(user32.GetSystemMetrics(1) - (user32.GetSystemMetrics(1)*10)/100)))  # x, y, w, h.
+    img = ImageGrab.grab(bbox=(
+        int(user32.GetSystemMetrics(0) / 2), int(0 + (user32.GetSystemMetrics(1) * 10) / 100),
+        user32.GetSystemMetrics(0),
+        int(user32.GetSystemMetrics(1) - (user32.GetSystemMetrics(1) * 10) / 100)))  # x, y, w, h.
 
     master.iconify()
     img_np = np.array(img)
-    RGB_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-    cv2.imshow('image', RGB_img)
+    rgb_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    cv2.imshow('image', rgb_img)
 
     cv2.setMouseCallback('image', click_event)
     cv2.waitKey(0)
+
 
 def save_config():
     config['DEFAULT'] = {
         'intensita_pixel': intensita_pixel,
         'intensita_pixel_vicini': intensita_p_vicini,
         'number_of_screenshot': number_of_screenshot,
-        'ingame_seconds_per_screenshot' : ingame_seconds_per_screenshot,
+        'ingame_seconds_per_screenshot': ingame_seconds_per_screenshot,
         'x_iniziale': x_iniziale,
         'y_iniziale': y_iniziale,
         'x_finale': x_finale,
         'y_finale': y_finale,
-        'x_bar' : x_bar,
-        'y_bar' : y_bar
+        'x_bar': x_bar,
+        'y_bar': y_bar
     }
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
+
 def readconfig():
-        global x_iniziale, x_finale, y_finale, y_iniziale, intensita_pixel, intensita_p_vicini, ingame_seconds_per_screenshot, number_of_screenshot, x_bar, y_bar
-        if glob.glob(CONF_FILE):
-            config.read(CONF_FILE)
-            intensita_pixel = int (config['DEFAULT']['intensita_pixel'])
-            intensita_p_vicini = int (config['DEFAULT']['intensita_pixel_vicini'])
-            ingame_seconds_per_screenshot = int(config['DEFAULT']['ingame_seconds_per_screenshot'])
-            number_of_screenshot = int(config['DEFAULT']['number_of_screenshot'])
-            x_iniziale = int(config['DEFAULT']['x_iniziale'])
-            y_iniziale = int(config['DEFAULT']['y_iniziale'])
-            x_finale = int(config['DEFAULT']['x_finale'])
-            y_finale = int(config['DEFAULT']['y_finale'])
-            x_bar = int(config['DEFAULT']['x_bar'])
-            y_bar = int(config['DEFAULT']['y_bar'])
-        else:
-            print("NO CONFIG FILE, USING THE DEFAULT ONES")
+    global x_iniziale, x_finale, y_finale, y_iniziale, intensita_pixel, intensita_p_vicini, ingame_seconds_per_screenshot, number_of_screenshot, x_bar, y_bar
+    if glob.glob(CONF_FILE):
+        config.read(CONF_FILE)
+        intensita_pixel = int(config['DEFAULT']['intensita_pixel'])
+        intensita_p_vicini = int(config['DEFAULT']['intensita_pixel_vicini'])
+        ingame_seconds_per_screenshot = int(config['DEFAULT']['ingame_seconds_per_screenshot'])
+        number_of_screenshot = int(config['DEFAULT']['number_of_screenshot'])
+        x_iniziale = int(config['DEFAULT']['x_iniziale'])
+        y_iniziale = int(config['DEFAULT']['y_iniziale'])
+        x_finale = int(config['DEFAULT']['x_finale'])
+        y_finale = int(config['DEFAULT']['y_finale'])
+        x_bar = int(config['DEFAULT']['x_bar'])
+        y_bar = int(config['DEFAULT']['y_bar'])
+    else:
+        print("NO CONFIG FILE, USING THE DEFAULT ONES")
+
 
 def lettura_immagini(heatmap):
-
     for movimento in glob.glob('elab_movimenti/*.png'):
         image = cv2.imread(movimento)
         image = cv2.resize(image, (x_finale - x_iniziale, y_finale - y_iniziale))
@@ -172,27 +176,29 @@ def lettura_immagini(heatmap):
 
         intensita(heatmap, coord)
 
+
 def intensita(array, coord):
     for c in coord:
 
         y = c[0][0]
-        x= c[0][1]
+        x = c[0][1]
 
         array[x, y] += intensita_pixel
         try:
             for s in range(20):
-                array[x+s, y] += intensita_p_vicini
-                array[x, y+s] += intensita_p_vicini
-                array[x-s, y] += intensita_p_vicini
-                array[x, y-s] += intensita_p_vicini
+                array[x + s, y] += intensita_p_vicini
+                array[x, y + s] += intensita_p_vicini
+                array[x - s, y] += intensita_p_vicini
+                array[x, y - s] += intensita_p_vicini
         except:
             pass
+
 
 def generazione_heatmap():
     heatmap = np.zeros((y_finale - y_iniziale, x_finale - x_iniziale))
     lettura_immagini(heatmap)
     heatmapshow = None
-    heatmapshow=heatmap*20
+    heatmapshow = heatmap * 20
     heatmapshow = cv2.normalize(heatmap, heatmapshow, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
     heatmapshow = cv2.applyColorMap(heatmapshow, cv2.COLORMAP_INFERNO)
 
@@ -206,28 +212,34 @@ def generazione_heatmap():
 
     return heatmapshow
 
+
 def set_Time_Screen(ts):
     global ingame_seconds_per_screenshot
     ingame_seconds_per_screenshot = ts
     save_config()
+
 
 def set_Number_Screenshot(ns):
     global number_of_screenshot
     number_of_screenshot = ns
     save_config()
 
+
 def set_Pixel_Intensity(PI):
     global intensita_pixel
     intensita_pixel = PI
     save_config()
+
 
 def set_Around_Pixel_Intensity(A_P_I):
     global intensita_p_vicini
     intensita_p_vicini = A_P_I
     save_config()
 
+
 def organize_images():
     source_folder = "movimenti"
     path = "elab_movimenti"
     shutil.rmtree(path)
-    shutil.copytree(source_folder, path,dirs_exist_ok=True)
+    shutil.copytree(source_folder, path, dirs_exist_ok=True)
+    text_files = glob.glob(source_folder + "/**/*.png", recursive=True)
